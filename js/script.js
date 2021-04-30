00de76a56d4baceb214ac5301423c0f659f9a4ba
karin valcanover <valcanoverkarin@outlook.it>
2021-04-30 10:24:46 +0200

    inizio milestone 2, ho aggiunto le bandiere

--- js/script.js
+++ js/script.js
@@ -6,12 +6,24 @@ var app = new Vue (
             api_key: '7256d088cd76b30a936046b2d338fc9d',
             //1b. aggiungo seachInput per la ricerca dell' utente nella imput
             searchInput: '',
-            //1g. creao l'Array che viene popolato dalla chiamata all'API
-            resultMovie: []
+            //1g. creao l'array che viene popolato dalla chiamata all'API
+            resultMovie: [],
+            //2a. creo l'array di link delle bandiere
+            flagsImages: {
+                en: 'https://lipis.github.io/flag-icon-css/flags/4x3/gb.svg',
+                it: 'https://lipis.github.io/flag-icon-css/flags/4x3/it.svg',
+                fr: 'https://lipis.github.io/flag-icon-css/flags/4x3/fr.svg',  
+                de: 'https://lipis.github.io/flag-icon-css/flags/4x3/de.svg',
+                es: 'https://lipis.github.io/flag-icon-css/flags/4x3/es.svg', 
+                da: 'https://lipis.github.io/flag-icon-css/flags/4x3/dk.svg',
+                pl: 'https://lipis.github.io/flag-icon-css/flags/4x3/pl.svg',
+                sv: 'https://lipis.github.io/flag-icon-css/flags/4x3/se.svg',
+                tr: 'https://lipis.github.io/flag-icon-css/flags/4x3/tr.svg'                           
+            }            
             
         },
         methods: {
-            //1d. creo la funzione che manda una chiamata all'API quando l'utente preme il button o preme enter
+            //1d. creo la funzione per popolare l'array resultMovie quando l'utente clicca la lente o preme enter
             searchFilm() {
                 axios
                 ////1e. aggiungo l' apposita API 
